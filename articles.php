9a3264880dcc0619e06e7f4a1c171a65bd03d186
Raul-Cuba <raulge7@mail.ru>
2016-12-29 03:06:57 +0300

    новый коммент

--- articles.php
+++ articles.php
@@ -11,6 +11,7 @@
           
 
 	      <?php
+               //сначала вытыгиваем инфу по всем статьям
                include ("models/articles.php");
                $articles = articles_all(); 
                
